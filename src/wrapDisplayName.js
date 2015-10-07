a015c59e33d715feb71a9e8fb16bb1694271c7db
Andrew Clark <acdlite@me.com>
2015-10-06 23:59:52 -0700

    wrapDisplayName() doesn't need to be curried

--- src/wrapDisplayName.js
+++ src/wrapDisplayName.js
@@ -1,8 +1,7 @@
-import curry from 'lodash/function/curry';
 import getDisplayName from './getDisplayName';
 
 const wrapDisplayName = (BaseComponent, hocName) => (
   `${hocName}(${getDisplayName(BaseComponent)})`
 );
 
-export default curry(wrapDisplayName);
+export default wrapDisplayName;
