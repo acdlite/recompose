7734101d5b47ca8fa2fd8b7dfa8764c2cffb2aec
Andrew Clark <acdlite@me.com>
2015-10-11 14:21:11 -0700

    Fix search-replace

--- scripts/build-umd.sh
+++ scripts/build-umd.sh
@@ -5,4 +5,4 @@ BIN=node_modules/.bin
 $BIN/webpack src/index.js dist/recompose.min.js
 size=$($BIN/gzip-size dist/recompose.min.js | $BIN/pretty-bytes)
 echo "Total gzipped size: $size"
-sed -i '' "s/<!-- gzipped-size --> *[0-9\.]* kB *<!-- end -->/<!-- gzipped-size -->$size<!-- end -->/" README.md
+sed -i '' "s/The total gzipped size of the entire library is \*\*[0-9\.]* kB\*\*/The total gzipped size of the entire library is \*\*$size\*\*/" README.md
