f6c87d4f4af2713fd0dd15ee5d04eae834f93173
Ryan Wise <ryanmwise1@gmail.com>
2015-11-18 15:21:23 -0800

    WIP: TodoTextInput

--- examples/todomvc/components/TodoTextInput.js
+++ examples/todomvc/components/TodoTextInput.js
@@ -1,101 +1,17 @@
 import React, { Component, PropTypes } from 'react'
 import classnames from 'classnames'
+import {
+  compose,
+  setPropTypes,
+  mapProps,
+  withState,
+  onlyUpdateForPropTypes
+} from 'recompose'
 
+const TodoTextInput = props =>
+  <input type="text" autoFocus="true" {...props} />
 
-
-// import {
-//   compose,
-//   setPropTypes,
-//   mapProps,
-//   withProps,
-//   renameProp,
-//   withState
-// } from 'recompose'
-
-// const TodoTextInput = { editing, newTodo, ...rest } =>
-//   <input
-//     className={classnames({ edit: editing, 'new-todo': newTodo })}
-//     type="text"
-//     autoFocus="true"
-//     {...rest} />
-
-// todoTextInputApi = {
-//   onSave: PropTypes.func.isRequired,
-//   text: PropTypes.string,
-//   placeholder: PropTypes.string,
-//   editing: PropTypes.bool,
-//   newTodo: PropTypes.bool
-// }
-
-// export default compose(
-//   setPropTypes(todoTextInputApi),
-//   withState('text', 'setText', ({ text }) => text || ''),
-//   mapProps(({ setText, text, newTodo, onSave, ...rest }) => ({
-//     value: text,
-//     onBlur: e => { if (!newTodo) onSave(e.target.value) },
-//     onChange: e => setText(e.target.value),
-//     onKeyDown: e => {
-//       if (e.which === 13) {
-//         onSave(e.target.value.trim())
-//         if (newTodo) {
-//           setText('')
-//         }
-//       }
-//     },
-//     ...rest
-//   }))
-// )(TodoTextInput)
-
-
-
-
-class TodoTextInput extends Component {
-  constructor(props, context) {
-    super(props, context)
-    this.state = {
-      text: this.props.text || ''
-    }
-  }
-
-  handleSubmit(e) {
-    const text = e.target.value.trim()
-    if (e.which === 13) {
-      this.props.onSave(text)
-      if (this.props.newTodo) {
-        this.setState({ text: '' })
-      }
-    }
-  }
-
-  handleChange(e) {
-    this.setState({ text: e.target.value })
-  }
-
-  handleBlur(e) {
-    if (!this.props.newTodo) {
-      this.props.onSave(e.target.value)
-    }
-  }
-
-  render() {
-    return (
-      <input className={
-        classnames({
-          edit: this.props.editing,
-          'new-todo': this.props.newTodo
-        })}
-        type="text"
-        placeholder={this.props.placeholder}
-        autoFocus="true"
-        value={this.state.text}
-        onBlur={this.handleBlur.bind(this)}
-        onChange={this.handleChange.bind(this)}
-        onKeyDown={this.handleSubmit.bind(this)} />
-    )
-  }
-}
-
-TodoTextInput.propTypes = {
+const todoTextInputApi = {
   onSave: PropTypes.func.isRequired,
   text: PropTypes.string,
   placeholder: PropTypes.string,
@@ -103,4 +19,20 @@ TodoTextInput.propTypes = {
   newTodo: PropTypes.bool
 }
 
-export default TodoTextInput
+export default compose(
+  onlyUpdateForPropTypes,
+  setPropTypes(todoTextInputApi),
+  withState('text', 'setText', ({ text }) => text || ''),
+  mapProps(({ setText, text, newTodo, onSave, editing, ...rest }) => ({
+    ...rest,
+    value: text,
+    className: classnames({ edit: editing, 'new-todo': newTodo }),
+    onBlur: e => { if (!newTodo) onSave(e.target.value) },
+    onChange: e => setText(e.target.value),
+    onKeyDown: e => {
+      const returnPressed = e.which === 13
+      if (returnPressed) onSave(e.target.value.trim())
+      if (returnPressed && newTodo) setText('')
+    }
+  }))
+)(TodoTextInput)
