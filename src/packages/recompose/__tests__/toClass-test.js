856ec4829ba765e8292e9e2696d219083b11f165
Andrew Clark <acdlite@me.com>
2015-12-30 15:01:31 -0800

    toClass should work with strings

--- src/packages/recompose/__tests__/toClass-test.js
+++ src/packages/recompose/__tests__/toClass-test.js
@@ -6,7 +6,7 @@ import createSpy from 'recompose/createSpy'
 import { renderIntoDocument } from 'react-addons-test-utils'
 
 describe('toClass()', () => {
-  it('should return the base component if it is already a class', () => {
+  it('returns the base component if it is already a class', () => {
     class BaseComponent extends React.Component {
       render() {
         return <div />
@@ -29,19 +29,19 @@ describe('toClass()', () => {
 
   const TestComponent = toClass(StatelessComponent)
 
-  it('should copy propTypes, displayName, contextTypes and defaultProps from base component', () => {
+  it('copies propTypes, displayName, contextTypes and defaultProps from base component', () => {
     expect(TestComponent.displayName).to.equal('Stateless')
     expect(TestComponent.propTypes).to.eql({ foo: PropTypes.string })
     expect(TestComponent.contextTypes).to.eql({ bar: PropTypes.object })
     expect(TestComponent.defaultProps).to.eql({ foo: 'bar', fizz: 'buzz' })
   })
 
-  it('should pass defaultProps correctly', () => {
+  it('passes defaultProps correctly', () => {
     renderIntoDocument(<TestComponent />)
     expect(spy.getProps().props).to.eql({ foo: 'bar', fizz: 'buzz' })
   })
 
-  it('should pass context and props correctly', () => {
+  it('passes context and props correctly', () => {
     const store = {}
 
     class Provider extends React.Component {
@@ -71,5 +71,10 @@ describe('toClass()', () => {
     expect(spy.getProps().context.bar).to.equal(store)
     expect(spy.getProps().props.fizz).to.equal('fizzbuzz')
 
+  })
+
+  it('works with strings (DOM components)', () => {
+    const Div = toClass('div')
+    renderIntoDocument(<Div>Hello</Div>)
   })
 })
