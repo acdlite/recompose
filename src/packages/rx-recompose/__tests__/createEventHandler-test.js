3e459937c352ca6dbbe7f3da05d357cf2dfc7b98
Dral <m.c.dral@gmail.com>
2016-02-24 01:15:26 -0300

    Make createEventHandler accept an optional function for cancelling events

--- src/packages/rx-recompose/__tests__/createEventHandler-test.js
+++ src/packages/rx-recompose/__tests__/createEventHandler-test.js
@@ -14,4 +14,23 @@ describe('createEventHandler()', () => {
     subscription.dispose()
     expect(result).to.eql([1, 2, 3])
   })
+
+  it('calls the function passed in and returns its return value', () => {
+    const result = []
+    const eventHandler = createEventHandler(e => {
+      e.preventDefault()
+      return false
+    })
+    const subscription = eventHandler.subscribe(v => result.push(v))
+    let preventDefaultCalled = false
+    const event = {
+      preventDefault: () => {
+        preventDefaultCalled = true
+      }
+    }
+    expect(eventHandler(event)).to.eql(false)
+    expect(preventDefaultCalled).to.eql(true)
+    subscription.dispose()
+    expect(result).to.eql([event])
+  })
 })
