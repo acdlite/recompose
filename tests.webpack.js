6ea435e7aa73181a40f462808d9e6fd0f966d520
Andrew Clark <acdlite@me.com>
2016-04-14 22:11:44 -0700

    Stop using Relay plugins

--- tests.webpack.js
+++ tests.webpack.js
@@ -1,8 +1,3 @@
 import 'babel-polyfill'
-import expect from 'expect'
-import expectJSX from 'expect-jsx'
-
-expect.extend(expectJSX)
-
 const context = require.context('./src', true, /-test\.js$/)
 context.keys().forEach(context)
