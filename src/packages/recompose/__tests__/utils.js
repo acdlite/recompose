eea7337507bc10d27cbfcd160a63f19df0d39d23
Andrew Clark <acdlite@me.com>
2016-04-14 22:10:06 -0700

    Remove unneeded modules

--- src/packages/recompose/__tests__/utils.js
+++ src/packages/recompose/__tests__/utils.js
@@ -1,55 +1,18 @@
 import React from 'react'
-import wrapDisplayName from '../wrapDisplayName'
-import { createRenderer } from 'react-addons-test-utils'
+import createHelper from '../createHelper'
 
-export class BaseComponent extends React.Component {
-  renderCount = 0;
-
-  render() {
-    return <div {...this.props}/>
-  }
-}
-
-export const shallowRender = (node, renderer = createRenderer()) => {
-  renderer.render(node)
-  return renderer.getRenderOutput()
-}
-
-export const countRenders = BaseComponent2 => (
+export const countRenders = createHelper(BaseComponent =>
   class extends React.Component {
-    static displayName = wrapDisplayName(BaseComponent2, 'countRenders');
     renderCount = 0;
 
     render() {
       this.renderCount += 1
       return (
-        <BaseComponent2
+        <BaseComponent
           renderCount={this.renderCount}
           {...this.props}
         />
       )
     }
   }
-)
-
-export class NullComponent extends React.Component {
-  render() {
-    return null
-  }
-}
-
-export const addForceUpdate = BaseComponent2 => (
-  class extends React.Component {
-    static displayName = wrapDisplayName(BaseComponent2);
-    forceUpdate = this.forceUpdate;
-
-    render() {
-      const props = {
-        ...this.props,
-        forceUpdate: this.forceUpdate
-      }
-
-      return <BaseComponent2 {...props} />
-    }
-  }
-)
+, 'countRenders', true, true)
