3e35a965b4d05588a6f12e05282fef060fb4063f
Andrew Clark <acdlite@me.com>
2016-04-27 17:09:49 -0700

    Port test from rx-recompose-next branch

--- src/packages/rx-recompose/__tests__/createEventHandler-test.js
+++ src/packages/rx-recompose/__tests__/createEventHandler-test.js
@@ -13,3 +13,21 @@ test('createEventHandler creates a subject that broadcasts new values when calle
   subscription.dispose()
   t.deepEqual(result, [1, 2, 3])
 })
+
+test('handles multiple subscribers', t => {
+  const result1 = []
+  const result2 = []
+  const { handler, stream } = createEventHandler()
+  const subscription1 = stream.subscribe(v => result1.push(v))
+  const subscription2 = stream.subscribe(v => result2.push(v))
+
+  handler(1)
+  handler(2)
+  handler(3)
+
+  subscription1.dispose()
+  subscription2.dispose()
+
+  t.deepEqual(result1, [1, 2, 3])
+  t.deepEqual(result2, [1, 2, 3])
+})
