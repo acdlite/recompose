9892f2b45bed5f87fa8eccc03d4f759ba705b775
Andrew Clark <acdlite@me.com>
2016-05-10 14:31:01 -0700

    Use a loop intead of reduce

--- src/packages/recompose/utils/pick.js
+++ src/packages/recompose/utils/pick.js
@@ -1,7 +1,12 @@
-const pick = (obj, keys) =>
-  keys.reduce((result, key) => {
-    result[key] = obj[key]
-    return result
-  }, {})
+const pick = (obj, keys) => {
+  const result = {}
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i]
+    if (obj.hasOwnProperty(key)) {
+      result[key] = obj[key]
+    }
+  }
+  return result
+}
 
 export default pick
