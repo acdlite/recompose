0e2e46afb157aebc5acd0326675cf2812e457797
Andrew Clark <acdlite@me.com>
2016-05-11 23:27:40 -0700

    Remove remaining lodash dep (#155)
    * Remove remaining lodash dep
    
    * Add tests for compose
    
    * Use loop instead of reduce

--- src/packages/recompose/compose.js
+++ src/packages/recompose/compose.js
@@ -1,3 +1,19 @@
-import flowRight from 'lodash/flowRight'
+export default function compose(...funcs) {
+  if (funcs.length === 0) {
+    return arg => arg
+  }
 
-export default flowRight
+  if (funcs.length === 1) {
+    return funcs[0]
+  }
+
+  const last = funcs[funcs.length - 1]
+  return (...args) => {
+    let result = last(...args)
+    for (let i = funcs.length - 2; i >= 0; i--) {
+      const f = funcs[i]
+      result = f(result)
+    }
+    return result
+  }
+}
