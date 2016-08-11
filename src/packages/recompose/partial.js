fe11c4227b566bf133fb6196e0f4e36a33504488
Aleksandr Aibulatov <zap.aibulatov@gmail.com>
2016-08-11 10:54:11 +0500

    Use arrow function to declare partial

--- src/packages/recompose/partial.js
+++ src/packages/recompose/partial.js
@@ -1,19 +1,17 @@
 import createHelper from './createHelper'
 import withHandlers from './withHandlers'
 
-function partial(functionName, funcArgs) {
-  return BaseComponent => withHandlers({
-    [functionName]: props => {
-      if (typeof props[functionName] !== 'function') {
-        const type = typeof props[functionName]
-        throw new Error(
-          `partial(): ${functionName} should be a function not ${type}`
-        )
-      }
-
-      return (...args) => props[functionName](...funcArgs, ...args)
+const partial = (functionName, funcArgs) => BaseComponent => withHandlers({
+  [functionName]: props => {
+    if (typeof props[functionName] !== 'function') {
+      const type = typeof props[functionName]
+      throw new Error(
+        `partial(): ${functionName} should be a function not ${type}`
+      )
     }
-  })(BaseComponent)
-}
+
+    return (...args) => props[functionName](...funcArgs, ...args)
+  }
+})(BaseComponent)
 
 export default createHelper(partial, 'partial')
