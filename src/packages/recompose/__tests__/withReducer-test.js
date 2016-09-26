0bb680fee7d90648faa9ab57c09c314e75623a4e
Boris Serdiuk <just-boris@hotmail.com>
2016-09-26 17:41:15 +0200

    withReducer: optionally initialize state from default reducer argument fixes #247

--- src/packages/recompose/__tests__/withReducer-test.js
+++ src/packages/recompose/__tests__/withReducer-test.js
@@ -3,15 +3,15 @@ import React from 'react'
 import { withReducer, compose, flattenProp } from '../'
 import { mount } from 'enzyme'
 
+const SET_COUNTER = 'SET_COUNTER'
+
 test('adds a stateful value and a function for updating it', t => {
-  const SET_COUNTER = 'SET_COUNTER'
+  const initialState = { counter: 0 }
 
   const reducer = (state, action) =>
     action.type === SET_COUNTER
       ? { counter: action.payload }
       : state
-
-  const initialState = { counter: 0 }
 
   const Counter = compose(
     withReducer(
@@ -32,4 +32,49 @@ test('adds a stateful value and a function for updating it', t => {
 
   dispatch({ type: SET_COUNTER, payload: 18 })
   t.is(div.prop('counter'), 18)
+})
+
+test('calls initialState when it is a function', t => {
+  const initialState = ({ initialCount }) => ({ counter: initialCount })
+
+  const reducer = (state, action) =>
+    action.type === SET_COUNTER
+      ? { counter: action.payload }
+      : state
+
+  const Counter = compose(
+    withReducer(
+      'state',
+      'dispatch',
+      reducer,
+      initialState
+    ),
+    flattenProp('state')
+  )('div')
+
+  const div = mount(<Counter initialCount={10} />).find('div')
+
+  t.is(div.prop('counter'), 10)
+})
+
+test('receives state from reducer when initialState is not provided', t => {
+  const initialState = { counter: 0 }
+
+  const reducer = (state = initialState, action) =>
+    action.type === SET_COUNTER
+      ? { counter: action.payload }
+      : state
+
+  const Counter = compose(
+    withReducer(
+      'state',
+      'dispatch',
+      reducer
+    ),
+    flattenProp('state')
+  )('div')
+
+  const div = mount(<Counter />).find('div')
+
+  t.is(div.prop('counter'), 0)
 })
