1c3d2b38edb394db1144af9f078a73008c612f7f
JoeCortopassi <joe@joecortopassi.com>
2016-12-13 13:27:15 -0800

    Simplified the composer to use higher order components (#293)

--- src/packages/recompose/compose.js
+++ src/packages/recompose/compose.js
@@ -7,13 +7,5 @@ export default function compose(...funcs) {
     return funcs[0]
   }
 
-  const last = funcs[funcs.length - 1]
-  return (...args) => {
-    let result = last(...args)
-    for (let i = funcs.length - 2; i >= 0; i--) {
-      const f = funcs[i]
-      result = f(result)
-    }
-    return result
-  }
+  return funcs.reduce((a, b) => (...args) => a(b(...args)))
 }
