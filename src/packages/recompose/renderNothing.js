9af90951217ca5b4ee81d0bbb894767a369ff66d
Ivan Starkov <istarkov@gmail.com>
2016-12-20 21:13:05 +0300

    Make renderNothing as a classComponent (#289)
    * Fixes #288
    
    * Test on render null

--- src/packages/recompose/renderNothing.js
+++ src/packages/recompose/renderNothing.js
@@ -1,9 +1,14 @@
+import { Component } from 'react'
 import createHelper from './createHelper'
 
-const renderNothing = _ => {
-  const Nothing = () => null
-  Nothing.displayName = 'Nothing'
-  return Nothing
+class Nothing extends Component {
+  render() {
+    return null
+  }
 }
+
+Nothing.displayName = 'Nothing'
+
+const renderNothing = _ => Nothing
 
 export default createHelper(renderNothing, 'renderNothing', false, true)
