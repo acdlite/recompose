f88218b38b5c41e69840c1e4abc091b9a5b2f00f
Bogdan Chadkin <trysound@yandex.ru>
2017-04-11 21:16:44 +0300

    Fix lint

--- src/packages/recompose/shallowEqual.js
+++ src/packages/recompose/shallowEqual.js
@@ -1,16 +1,16 @@
 const shallowEqual = (a, b) => {
   if (a === b) {
-    return true;
+    return true
   }
   if (!a || !b) {
-    return false;
+    return false
   }
-  const keysA = Object.keys(a);
-  const keysB = Object.keys(b);
+  const keysA = Object.keys(a)
+  const keysB = Object.keys(b)
   if (keysA.length !== keysB.length) {
-    return false;
+    return false
   }
-  return keysA.every(key => a[key] === b[key]);
-};
+  return keysA.every(key => a[key] === b[key])
+}
 
 export default shallowEqual
