adc54d16298ca97a20b1b34c781be5852a1d4dc2
CT.Wu <chingting.wu@appier.com>
2017-05-03 14:30:43 +0800

    Fix lint

--- src/packages/recompose/setObservableConfig.js
+++ src/packages/recompose/setObservableConfig.js
@@ -9,13 +9,13 @@ const configureObservable = c => {
 
 export const config = {
   fromESObservable: observable =>
-    (typeof _config.fromESObservable === 'function'
+    typeof _config.fromESObservable === 'function'
       ? _config.fromESObservable(observable)
-      : observable),
+      : observable,
   toESObservable: stream =>
-    (typeof _config.toESObservable === 'function'
+    typeof _config.toESObservable === 'function'
       ? _config.toESObservable(stream)
-      : stream),
+      : stream,
 }
 
 export default configureObservable
