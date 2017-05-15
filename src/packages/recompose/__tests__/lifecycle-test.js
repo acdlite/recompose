a74d7c0013d4b792ed8b9e6b17f68ff9db25127f
Joep van der Heijden <jvdheijden90@gmail.com>
2017-05-15 11:18:08 +0200

    Change createClass to Component in docs (#374)

--- src/packages/recompose/__tests__/lifecycle-test.js
+++ src/packages/recompose/__tests__/lifecycle-test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { mount } from 'enzyme'
 import { lifecycle } from '../'
 
-test('lifecycle is a higher-order component version of React.createClass', () => {
+test('lifecycle is a higher-order component version of React.Component', () => {
   const enhance = lifecycle({
     componentWillMount() {
       this.setState({ 'data-bar': 'baz' })
