d1adfa6d0de4ef3ce5fca12ff32c313880ff7cd5
jjant <jantonielli@itba.edu.ar>
2017-06-22 21:56:01 -0300

    Remove special cases from compose

--- src/packages/recompose/compose.js
+++ src/packages/recompose/compose.js
@@ -1,11 +1,4 @@
 export default function compose(...funcs) {
-  if (funcs.length === 0) {
-    return arg => arg
-  }
-
-  if (funcs.length === 1) {
-    return funcs[0]
-  }
-
-  return funcs.reduce((a, b) => (...args) => a(b(...args)))
+  const identity = arg => arg
+  return funcs.reduce((a, b) => (...args) => a(b(...args)), identity)
 }
