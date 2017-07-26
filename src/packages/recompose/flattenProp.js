c29cd62b94ce617248e2f9b539a9104bfd8c8fc6
donavon <github@donavon.com>
2017-07-26 09:52:34 -0400

    flattenProp accepts an optional keys arg
    If specified (as an Array), flattens only the keys specified.

--- src/packages/recompose/flattenProp.js
+++ src/packages/recompose/flattenProp.js
@@ -1,13 +1,14 @@
 import setDisplayName from './setDisplayName'
 import wrapDisplayName from './wrapDisplayName'
 import createEagerFactory from './createEagerFactory'
+import pick from './utils/pick'
 
-const flattenProp = propName => BaseComponent => {
+const flattenProp = (propName, keys) => BaseComponent => {
   const factory = createEagerFactory(BaseComponent)
   const FlattenProp = props =>
     factory({
       ...props,
-      ...props[propName],
+      ...(keys ? pick(props[propName], keys) : props[propName]),
     })
 
   if (process.env.NODE_ENV !== 'production') {
