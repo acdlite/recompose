737150d72e6131d5e1e7bc2f5bb1e58ffe6eb21d
Ivan Starkov <istarkov@gmail.com>
2017-08-24 15:45:21 +0300

    Add flow tests for user enhancers in flow libdef (#493)

---
+++ types/flow-typed/recompose_v0.24.x/flow_v0.53.x-/test_classBasedEnhancer.js
@@ -0,0 +1,45 @@
+/* @flow */
+import * as React from 'react'
+import { compose, withProps } from 'recompose'
+import type { HOC } from 'recompose'
+
+// Example of very dirty written fetcher enhancer
+function fetcher<Response: {}, Base: {}>(
+  dest: string,
+  nullRespType: ?Response
+): HOC<{ ...$Exact<Base>, data?: Response }, Base> {
+  return BaseComponent =>
+    class Fetcher extends React.Component<Base, { data?: Response }> {
+      state = { data: undefined }
+      componentDidMount() {
+        fetch(dest)
+          .then(r => r.json())
+          .then((data: Response) => this.setState({ data }))
+      }
+      render() {
+        return <BaseComponent {...this.props} {...this.state} />
+      }
+    }
+}
+// Enhanced Component props type
+type EnhancedCompProps = { b: number }
+// response type
+type FetchResponseType = { hello: string, world: number }
+
+// Now you can use it, let's check
+const enhancer: HOC<*, EnhancedCompProps> = compose(
+  // pass response type via typed null
+  fetcher('http://endpoint.ep', (null: ?FetchResponseType)),
+  // see here fully typed data
+  withProps(({ data }) => {
+    if (data !== undefined) {
+      return {
+        h: (data.hello: string),
+        // $ExpectError
+        hE: (data.hello: number),
+      }
+    }
+
+    return {}
+  })
+)
