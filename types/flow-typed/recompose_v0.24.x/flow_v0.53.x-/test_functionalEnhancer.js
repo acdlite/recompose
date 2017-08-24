45b330e07fa8f725f19c8113f813c7e424a42b37
cybice <istarkov@gmail.com>
2017-08-24 15:20:34 +0300

    Add flow tests for user enhancers in flow libdef

---
+++ types/flow-typed/recompose_v0.24.x/flow_v0.53.x-/test_functionalEnhancer.js
@@ -0,0 +1,44 @@
+/* @flow */
+
+import * as React from 'react'
+import { compose, withProps } from 'recompose'
+import type { HOC } from 'recompose'
+
+function mapProps<BaseProps: {}, EnhancedProps>(
+  mapperFn: EnhancedProps => BaseProps
+): (React.ComponentType<BaseProps>) => React.ComponentType<EnhancedProps> {
+  return Component => props => <Component {...mapperFn(props)} />
+}
+
+type EnhancedProps = { hello: string }
+
+const baseComponent = ({ hello, len }) =>
+  <div>
+    {(hello: string)}
+
+    {
+      // $ExpectError
+      (hello: number)
+    }
+
+    {(len: number)}
+
+    {
+      // $ExpectError
+      (len: string)
+    }
+  </div>
+
+const enhancer: HOC<*, EnhancedProps> = compose(
+  mapProps(({ hello }) => ({
+    hello: `${hello} world`,
+    len: hello.length,
+  })),
+  withProps(props => ({
+    helloAndLen: `${props.hello} ${props.len}`,
+    // $ExpectError
+    lE: (props.len: string),
+  }))
+)
+
+enhancer(baseComponent)
