d7923b6f1557f129baa12998ea5e5a779ae76190
Spencer <spalger@users.noreply.github.com>
2017-12-16 00:15:29 -0700

    filter packages by the existence of a package.json file (#575)

--- scripts/getPackageNames.js
+++ scripts/getPackageNames.js
@@ -8,11 +8,18 @@ let names
 
 exports.getPackageNames = () => {
   if (!names) {
-    names = fs
-      .readdirSync(exports.PACKAGES_SRC_DIR)
-      .filter(file =>
-        fs.statSync(path.resolve(exports.PACKAGES_SRC_DIR, file)).isDirectory()
-      )
+    names = fs.readdirSync(exports.PACKAGES_SRC_DIR).filter(file => {
+      try {
+        const packageJsonPath = path.resolve(
+          exports.PACKAGES_SRC_DIR,
+          file,
+          'package.json'
+        )
+        return fs.statSync(packageJsonPath).isFile()
+      } catch (error) {
+        return false
+      }
+    })
   }
   return names
 }
