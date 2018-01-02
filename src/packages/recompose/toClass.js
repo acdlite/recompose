8cb0d5fa4b38387ce2b75e003f267aa6633e2920
Thomas Grainger <tagrain@gmail.com>
2018-01-02 15:15:16 +0000

    use static class properties to convert toClass implicit return arrow

--- src/packages/recompose/toClass.js
+++ src/packages/recompose/toClass.js
@@ -2,26 +2,19 @@ import React, { Component } from 'react'
 import getDisplayName from './getDisplayName'
 import isClassComponent from './isClassComponent'
 
-const toClass = baseComponent => {
-  if (isClassComponent(baseComponent)) {
-    return baseComponent
-  }
-
-  class ToClass extends Component {
-    render() {
-      if (typeof baseComponent === 'string') {
-        return React.createElement(baseComponent, this.props)
+const toClass = baseComponent =>
+  isClassComponent(baseComponent)
+    ? baseComponent
+    : class ToClass extends Component {
+        static displayName = getDisplayName(baseComponent)
+        static propTypes = baseComponent.propTypes
+        static contextTypes = baseComponent.contextTypes
+        static defaultProps = baseComponent.defaultProps
+        render() {
+          if (typeof baseComponent === 'string') {
+            return React.createElement(baseComponent, this.props)
+          }
+          return baseComponent(this.props, this.context)
+        }
       }
-      return baseComponent(this.props, this.context)
-    }
-  }
-
-  ToClass.displayName = getDisplayName(baseComponent)
-  ToClass.propTypes = baseComponent.propTypes
-  ToClass.contextTypes = baseComponent.contextTypes
-  ToClass.defaultProps = baseComponent.defaultProps
-
-  return ToClass
-}
-
 export default toClass
