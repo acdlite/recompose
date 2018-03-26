ea27cfbc74966a6a663688da29da2f0d0e47c16e
pex <rernst@farbenmeer.net>
2018-03-26 12:39:28 +0200

    Pass blacklist argument to hoist-non-react-statics using hoistStatics

--- src/packages/recompose/__tests__/hoistStatics-test.js
+++ src/packages/recompose/__tests__/hoistStatics-test.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { createFactory } from 'react'
 import { mount } from 'enzyme'
 import sinon from 'sinon'
 import { hoistStatics, mapProps } from '../'
@@ -15,4 +15,18 @@ test('copies non-React static properties from base component to new component', 
 
   mount(<EnhancedComponent n={3} />)
   expect(BaseComponent.firstCall.args[0].n).toBe(15)
+})
+
+test('does not copy blacklisted static properties to new component ', () => {
+  const BaseComponent = sinon.spy(() => null)
+  BaseComponent.foo = () => {}
+  BaseComponent.bar = () => {}
+
+  const EnhancedComponent = hoistStatics(
+    comp => createFactory(comp),
+    { bar: true } // Blacklist
+  )(BaseComponent)
+
+  expect(EnhancedComponent.foo).toBe(BaseComponent.foo)
+  expect(EnhancedComponent.bar).toBe(undefined)
 })
