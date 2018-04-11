a108f64d2d940f058de9579cd45daf8c9b158a64
Bogdan Chadkin <trysound@yandex.ru>
2018-04-11 10:50:43 +0300

    Refactor rollup config (#645)
    - move all bundles into dist folder
    - add size snapshot
    - remove treeshake test

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -1,80 +1,88 @@
-const path = require('path')
-const nodeResolve = require('rollup-plugin-node-resolve')
-const babel = require('rollup-plugin-babel')
-const replace = require('rollup-plugin-replace')
-const commonjs = require('rollup-plugin-commonjs')
-const uglify = require('rollup-plugin-uglify')
-const { pascalCase } = require('change-case')
+import path from 'path'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+import replace from 'rollup-plugin-replace'
+import commonjs from 'rollup-plugin-commonjs'
+import uglify from 'rollup-plugin-uglify'
+import { sizeSnapshot } from 'rollup-plugin-size-snapshot'
+import { pascalCase } from 'change-case'
+
 const { PACKAGES_SRC_DIR, PACKAGES_OUT_DIR } = require('./getPackageNames')
 
-const build = process.env.BUILD
 const packageName = process.env.PACKAGE_NAME
+
 const libraryName = pascalCase(packageName)
-const sourceDir = path.resolve(PACKAGES_SRC_DIR, packageName)
-const outDir = path.resolve(PACKAGES_OUT_DIR, packageName)
 
-const config = {
-  entry: `${sourceDir}/index.js`,
-  external: ['react'],
-  globals: {
-    react: 'React',
-  },
-  moduleName: libraryName,
-  plugins: [
-    babel({
-      exclude: '**/node_modules/**',
-    }),
-  ],
-}
+const input = `./${path.join(PACKAGES_SRC_DIR, packageName, 'index.js')}`
 
-if (build === 'es' || build === 'cjs') {
-  config.external.push(
-    'fbjs/lib/shallowEqual',
-    'hoist-non-react-statics',
-    'change-emitter',
-    'symbol-observable',
-    'react-relay',
-    'recompose'
-  )
-  config.dest = `${outDir}/${build}/${libraryName}.js`
-  config.format = build
-}
+const outDir = path.join(PACKAGES_OUT_DIR, packageName, 'dist')
 
-if (build === 'umd') {
-  config.dest = `${outDir}/build/${libraryName}.js`
-  config.format = 'umd'
-  config.plugins.push(
-    nodeResolve({
-      jsnext: true,
-    }),
-    commonjs(),
-    replace({
-      'process.env.NODE_ENV': JSON.stringify('development'),
-    })
-  )
-}
+const isExternal = id => !id.startsWith('.') && !id.startsWith('/')
 
-if (build === 'min') {
-  config.dest = `${outDir}/build/${libraryName}.min.js`
-  config.format = 'umd'
-  config.plugins.push(
-    nodeResolve({
-      jsnext: true,
-    }),
-    commonjs(),
-    replace({
-      'process.env.NODE_ENV': JSON.stringify('production'),
-    }),
-    uglify({
-      compress: {
-        pure_getters: true,
-        unsafe: true,
-        unsafe_comps: true,
-        screw_ie8: true,
-        warnings: false,
+const getBabelOptions = () => ({
+  exclude: '**/node_modules/**',
+  runtimeHelpers: true,
+})
+
+export default [
+  {
+    input,
+    output: {
+      file: `${outDir}/${libraryName}.umd.js`,
+      format: 'umd',
+      name: libraryName,
+      globals: {
+        react: 'React',
       },
-    })
-  )
-}
+    },
+    external: ['react'],
+    plugins: [
+      nodeResolve(),
+      babel(getBabelOptions()),
+      commonjs(),
+      replace({ 'process.env.NODE_ENV': JSON.stringify('development') }),
+      sizeSnapshot(),
+    ],
+  },
 
-export default config
+  {
+    input,
+    output: {
+      file: `${outDir}/${libraryName}.min.js`,
+      format: 'umd',
+      name: libraryName,
+      globals: {
+        react: 'React',
+      },
+    },
+    external: ['react'],
+    plugins: [
+      nodeResolve(),
+      babel(getBabelOptions()),
+      commonjs(),
+      replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
+      sizeSnapshot(),
+      uglify(),
+    ],
+  },
+
+  {
+    input,
+    output: {
+      file: `${outDir}/${libraryName}.cjs.js`,
+      format: 'cjs',
+    },
+    external: isExternal,
+    plugins: [babel(getBabelOptions())],
+  },
+
+  {
+    input,
+    output: {
+      file: `${outDir}/${libraryName}.esm.js`,
+      format: 'es',
+    },
+    external: isExternal,
+    plugins: [babel(getBabelOptions()), sizeSnapshot()],
+  },
+]
