f011941527ad838e65f19bcda2e59ad4ecee0614
Ivan Starkov <istarkov@gmail.com>
2018-04-12 20:43:10 +0300

    Update dependencies before changes according to React 16.3+ (#646)

--- src/packages/recompose/__tests__/componentFromStream-test.js
+++ src/packages/recompose/__tests__/componentFromStream-test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { mount } from 'enzyme'
 import { Observable, Subject } from 'rxjs'
+import sinon from 'sinon'
 import rxjsConfig from '../rxjsObservableConfig'
 import { componentFromStreamWithConfig } from '../componentFromStream'
 
@@ -45,21 +46,25 @@ test('componentFromStream renders nothing until the stream emits a value', () =>
   const wrapper = mount(<Div />)
   expect(wrapper.find('div').length).toBe(0)
   vdom$.next()
+  wrapper.update()
   expect(wrapper.find('div').length).toBe(1)
 })
 
 test('handler multiple observers of props stream', () => {
+  const Other = () => <div />
   const Div = componentFromStream(props$ =>
     // Adds three observers to props stream
-    props$.combineLatest(props$, props$, props1 => <div {...props1} />)
+    props$.combineLatest(props$, props$, props1 => <Other {...props1} />)
   )
 
-  const wrapper = mount(<Div value={1} />)
-  const div = wrapper.find('div')
+  const wrapper = mount(<Div data-value={1} />)
+  const div = wrapper.find(Other)
 
-  expect(div.prop('value')).toBe(1)
-  wrapper.setProps({ value: 2 })
-  expect(div.prop('value')).toBe(2)
+  expect(div.prop('data-value')).toBe(1)
+  wrapper.setProps({ 'data-value': 2 })
+  wrapper.update()
+  const div2 = wrapper.find(Other)
+  expect(div2.prop('data-value')).toBe(2)
 })
 
 test('complete props stream before unmounting', () => {
@@ -100,5 +105,8 @@ test('completed props stream should throw an exception', () => {
 
   expect(wrapper.find('div').length).toBe(1)
 
+  const error = sinon.stub(console, 'error')
+
   expect(() => wrapper.unmount()).toThrowError(/no elements in sequence/)
+  expect(error.called).toBe(true)
 })
