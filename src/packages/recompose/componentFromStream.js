ede64b721e6a216d4a9921d38702871a9dbb8bef
Jonas Strømsodd <jonas.stromsodd@gmail.com>
2018-05-27 18:49:49 +0200

    Refactor cmponentFromStream to use create-subscription

--- src/packages/recompose/componentFromStream.js
+++ src/packages/recompose/componentFromStream.js
@@ -1,12 +1,46 @@
-import { Component } from 'react'
+import React, { Component } from 'react'
+import { createSubscription } from 'create-subscription'
 import { createChangeEmitter } from 'change-emitter'
 import $$observable from 'symbol-observable'
 import { config as globalConfig } from './setObservableConfig'
 
+class SimpleBehaviorSubject {
+  currentValue = null
+
+  constructor(observable) {
+    this.observable = observable
+  }
+
+  getCurrentValue() {
+    return this.currentValue
+  }
+
+  subscribe(callback) {
+    const subscription = this.observable.subscribe({
+      next: value => {
+        this.currentValue = value
+        callback(value)
+      },
+    })
+
+    return () => {
+      subscription.unsubscribe()
+    }
+  }
+}
+
+const Subscription = createSubscription({
+  subscribe(simpleSubject, callback) {
+    return simpleSubject.subscribe(callback)
+  },
+
+  getCurrentValue(simpleSubject) {
+    return simpleSubject.getCurrentValue()
+  },
+})
+
 export const componentFromStreamWithConfig = config => propsToVdom =>
   class ComponentFromStream extends Component {
-    state = { vdom: null }
-
     propsEmitter = createChangeEmitter()
 
     // Stream of props
@@ -29,17 +63,24 @@ export const componentFromStreamWithConfig = config => propsToVdom =>
     // Stream of vdom
     vdom$ = config.toESObservable(propsToVdom(this.props$))
 
-    componentDidMount() {
-      // Subscribe to child prop changes so we know when to re-render
-      this.subscription = this.vdom$.subscribe({
-        next: vdom => {
-          if (this.state.vdom !== vdom) {
-            this.setState({ vdom })
-          }
-        },
-      })
-      this.propsEmitter.emit(this.props)
+    constructor(props) {
+      super(props)
+
+      this.subscription = new SimpleBehaviorSubject(this.vdom$)
+      this.propsEmitter.emit(props)
     }
+
+    // componentDidMount() {
+    //   // Subscribe to child prop changes so we know when to re-render
+    //   this.subscription = this.vdom$.subscribe({
+    //     next: vdom => {
+    //       if (this.state.vdom !== vdom) {
+    //         this.setState({ vdom })
+    //       }
+    //     },
+    //   })
+    //   this.propsEmitter.emit(this.props)
+    // }
 
     componentDidUpdate(prevProps) {
       if (this.props !== prevProps) {
@@ -49,14 +90,17 @@ export const componentFromStreamWithConfig = config => propsToVdom =>
 
     componentWillUnmount() {
       // Call without arguments to complete stream
-      this.propsEmitter.emit()
-
+      // this.propsEmitter.emit()
       // Clean-up subscription before un-mounting
-      this.subscription.unsubscribe()
+      // this.subscription.unsubscribe()
     }
 
     render() {
-      return this.state.vdom
+      return (
+        <Subscription source={this.subscription}>
+          {vdom => vdom}
+        </Subscription>
+      )
     }
   }
 
