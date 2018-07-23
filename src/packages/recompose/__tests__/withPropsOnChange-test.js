416dd3322df6f3e1ee48a6b506d22b4fac2abb31
motiazu <moti@getvim.com>
2018-07-23 15:30:34 +0300

    Fix withPropsOnChange shouldMap consistency (#707)

--- src/packages/recompose/__tests__/withPropsOnChange-test.js
+++ src/packages/recompose/__tests__/withPropsOnChange-test.js
@@ -1,7 +1,13 @@
 import * as React from 'react'
 import { mount } from 'enzyme'
 import sinon from 'sinon'
-import { withPropsOnChange, withState, flattenProp, compose } from '../'
+import {
+  withPropsOnChange,
+  withState,
+  withStateHandlers,
+  flattenProp,
+  compose,
+} from '../'
 
 test('withPropsOnChange maps subset of owner props to child props', () => {
   const component = sinon.spy(() => null)
@@ -42,4 +48,73 @@ test('withPropsOnChange maps subset of owner props to child props', () => {
   expect(component.lastCall.args[0].c).toBe('baz')
   expect(component.calledThrice).toBe(true)
   expect(mapSpy.callCount).toBe(2)
+})
+
+test('withPropsOnChange maps subset of owner props to child props with custom predicate', () => {
+  const component = sinon.spy(() => null)
+  component.displayName = 'component'
+
+  const mapSpy = sinon.spy()
+  const shouldMapSpy = sinon.spy()
+  const PageContainer = compose(
+    withStateHandlers(
+      { result: { hasError: false, loading: true, error: null } },
+      {
+        updateResult: ({ result }) => payload => ({
+          result: { ...result, ...payload },
+        }),
+      }
+    ),
+    withPropsOnChange(
+      ({ result }, { result: nextResult }) => {
+        shouldMapSpy(result, nextResult)
+        return !result.hasError && nextResult.hasError
+      },
+      ({ result: { hasError, error } }) => {
+        mapSpy()
+
+        if (hasError) {
+          return {
+            errorEverHappened: true,
+            lastError: error,
+          }
+        }
+
+        return {
+          errorEverHappened: false,
+        }
+      }
+    )
+  )(component)
+
+  expect(PageContainer.displayName).toBe(
+    'withStateHandlers(withPropsOnChange(component))'
+  )
+
+  mount(<PageContainer />)
+  const { updateResult } = component.firstCall.args[0]
+  expect(component.lastCall.args[0].errorEverHappened).toBe(false)
+  expect(component.lastCall.args[0].lastError).toBeUndefined()
+  expect(component.calledOnce).toBe(true)
+  expect(mapSpy.callCount).toBe(1)
+  expect(shouldMapSpy.callCount).toBe(1)
+
+  updateResult({ loading: false, hasError: true, error: '1' })
+  expect(component.lastCall.args[0].errorEverHappened).toBe(true)
+  expect(component.lastCall.args[0].lastError).toBe('1')
+  expect(component.calledTwice).toBe(true)
+  expect(mapSpy.callCount).toBe(2)
+
+  // Does not re-map for false map result
+  updateResult({ loading: true, hasError: false, error: null })
+  expect(component.lastCall.args[0].errorEverHappened).toBe(true)
+  expect(component.lastCall.args[0].lastError).toBe('1')
+  expect(component.calledThrice).toBe(true)
+  expect(mapSpy.callCount).toBe(2)
+
+  updateResult({ loading: false, hasError: true, error: '2' })
+  expect(component.lastCall.args[0].errorEverHappened).toBe(true)
+  expect(component.lastCall.args[0].lastError).toBe('2')
+  expect(component.callCount).toBe(4)
+  expect(mapSpy.callCount).toBe(3)
 })
