454d1b5f653744f9d25fb7d46785ba4054050e45
Kamil Breguła <mik-laj@users.noreply.github.com>
2018-12-09 22:46:00 +0100

    Fix: Use ref forwarding

--- src/packages/recompose/defaultProps.js
+++ src/packages/recompose/defaultProps.js
@@ -1,10 +1,11 @@
-import { createFactory } from 'react'
+import { createElement } from 'react'
 import setDisplayName from './setDisplayName'
 import wrapDisplayName from './wrapDisplayName'
 
 const defaultProps = props => BaseComponent => {
-  const factory = createFactory(BaseComponent)
-  const DefaultProps = ownerProps => factory(ownerProps)
+  const DefaultProps = React.forwardRef((props, ref) =>
+    createElement(BaseComponent, { ...props, ref })
+  )
   DefaultProps.defaultProps = props
   if (process.env.NODE_ENV !== 'production') {
     return setDisplayName(wrapDisplayName(BaseComponent, 'defaultProps'))(
