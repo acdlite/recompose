785e710b166a59e168c9fa686533e814b78a9e03
Kamil Breguła <mik-laj@users.noreply.github.com>
2018-12-21 12:59:10 +0100

    Update defaultProps.js

--- src/packages/recompose/defaultProps.js
+++ src/packages/recompose/defaultProps.js
@@ -1,10 +1,10 @@
-import { createElement } from 'react'
+import React, { createElement } from 'react'
 import setDisplayName from './setDisplayName'
 import wrapDisplayName from './wrapDisplayName'
 
 const defaultProps = props => BaseComponent => {
-  const DefaultProps = React.forwardRef((props, ref) =>
-    createElement(BaseComponent, { ...props, ref })
+  const DefaultProps = React.forwardRef((childProps, ref) =>
+    createElement(BaseComponent, { ...childProps, ref })
   )
   DefaultProps.defaultProps = props
   if (process.env.NODE_ENV !== 'production') {
