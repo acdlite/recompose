84276a7d0a53fa59b8afe266448871c9b9cff5c8
Myron <myron.apostolakis@onapp.com>
2020-05-12 13:25:20 +0300

    replace create factory with create element

--- src/packages/recompose/mapProps.js
+++ src/packages/recompose/mapProps.js
@@ -1,10 +1,9 @@
-import { createFactory } from 'react'
+import { createElement } from 'react'
 import setDisplayName from './setDisplayName'
 import wrapDisplayName from './wrapDisplayName'
 
 const mapProps = propsMapper => BaseComponent => {
-  const factory = createFactory(BaseComponent)
-  console.log('minor', factory);
+  const factory = createElement(BaseComponent)
   const MapProps = props => factory(propsMapper(props))
   if (process.env.NODE_ENV !== 'production') {
     return setDisplayName(wrapDisplayName(BaseComponent, 'mapProps'))(MapProps)
