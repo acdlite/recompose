2c15245216c5dcb48684427a1c71b53f2592a700
Myron <myron.apostolakis@onapp.com>
2020-05-12 19:50:18 +0300

    minor

--- src/packages/recompose/mapProps.js
+++ src/packages/recompose/mapProps.js
@@ -3,7 +3,7 @@ import setDisplayName from './setDisplayName'
 import wrapDisplayName from './wrapDisplayName'
 
 const mapProps = propsMapper => BaseComponent => {
-  const factory = createElement(BaseComponent)
+  const factory = createElement(BaseComponent);
   const MapProps = props => factory(propsMapper(props))
   if (process.env.NODE_ENV !== 'production') {
     return setDisplayName(wrapDisplayName(BaseComponent, 'mapProps'))(MapProps)
