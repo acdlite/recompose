f86e858fc13a90b66759612e1d5b5951d119cab9
Myron <myron.apostolakis@onapp.com>
2020-05-13 17:59:06 +0300

    debugging

--- scripts/release.js
+++ scripts/release.js
@@ -50,6 +50,7 @@ try {
   const libraryName = pascalCase(packageName)
 
   const versionLoc = path.resolve(PACKAGES_SRC_DIR, packageName, 'VERSION')
+  console.log('versionLoc', versionLoc)
   const version = fs.readFileSync(versionLoc, 'utf8').trim()
 
   let nextVersion = readline.question(
